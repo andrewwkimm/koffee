64e0ec82269a8400a3a825d80065edbd7f46d522
Andrew Kim <andrewkimka@gmail.com>
2024-09-05 05:26:54 +0000

    Add proper type annotation

--- src/koffee/asr.py
+++ src/koffee/asr.py
@@ -1,6 +1,5 @@
 """Text extractor from audio."""
 
-import json
 from pathlib import Path
 from typing import Union
 
@@ -13,23 +12,23 @@ def transcribe_text(
     device="cpu",
     compute_type="float32",
     whisper_arch="large-v3",
-) -> json:
+) -> dict:
     """Transcribes text from a video file."""
     model = whisperx.load_model(
         whisper_arch=whisper_arch, device=device, compute_type=compute_type
     )
     audio = whisperx.load_audio(video_file)
-    result = model.transcribe(audio, batch_size=batch_size)
+    transcription = model.transcribe(audio, batch_size=batch_size)
 
     model_a, metadata = whisperx.load_align_model(
-        language_code=result["language"], device=device
+        language_code=transcription["language"], device=device
     )
-    aligned_result = whisperx.align(
-        result["segments"],
+    aligned_transcription = whisperx.align(
+        transcription["segments"],
         model_a,
         metadata,
         audio,
         device,
         return_char_alignments=False,
     )
-    return aligned_result
+    return aligned_transcription
