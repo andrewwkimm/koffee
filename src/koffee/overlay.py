6b50d9187156c2403353f5d1f675d704f435646b
Andrew Kim <andrewkimka@gmail.com>
2024-09-05 07:12:22 +0000

    Start subtitle overlayer

---
+++ src/koffee/overlay.py
@@ -0,0 +1,17 @@
+"""Subtitle overlayer."""
+
+from pathlib import Path
+from typing import Union
+
+import ffmpeg
+
+
+def overlay_subtitles(
+    video_file_path: Union[Path, str],
+    srt_path: Union[Path, str],
+    video_file_output_path: Union[Path, str],
+) -> None:
+    """Adds subtitles from an SRT file to a video file."""
+    ffmpeg.input(video_file_path).output(
+        video_file_output_path, vf=f"subtitles={srt_path}"
+    ).run(overwrite_output=True)
