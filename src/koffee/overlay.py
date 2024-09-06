19ada5fe8784db851a4cd44e1e8e4bc014864147
Andrew Kim <andrewkimka@gmail.com>
2024-09-06 03:20:42 +0000

    Start subtitle overlayer

--- src/koffee/overlay.py
+++ src/koffee/overlay.py
@@ -1,5 +1,6 @@
 """Subtitle overlayer."""
 
+import os
 from pathlib import Path
 from typing import Union
 
@@ -12,6 +13,11 @@ def overlay_subtitles(
     video_file_output_path: Union[Path, str],
 ) -> None:
     """Adds subtitles from an SRT file to a video file."""
+    # TODO: Investigate why setting output path as a Pathlib
+    #       object causes error when the other parameters don't
+
     ffmpeg.input(video_file_path).output(
-        video_file_output_path, vf=f"subtitles={srt_path}"
+        str(video_file_output_path), vf=f"subtitles={srt_path}"
     ).run(overwrite_output=True)
+
+    os.remove(srt_path)
