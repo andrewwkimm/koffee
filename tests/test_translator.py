d818d8f6af2fe6cf25300c38860955abe636c786
Andrew Kim <andrewkimka@gmail.com>
2024-09-07 03:06:58 +0000

    Test translations by transformers

---
+++ tests/test_translator.py
@@ -0,0 +1,22 @@
+"""Tests text translation."""
+
+from koffee.translator import translate_transcript
+
+
+def test_translate_transcript() -> None:
+    """Tests that the transcript was translated properly."""
+    sample_transcript = {
+        "segments": [
+            {"text": "안녕하세요."},
+            {"text": "음식."},
+        ],
+        "language": "ko",
+    }
+
+    actual = translate_transcript(sample_transcript)
+    expected = [
+        {"text": "Hello."},
+        {"text": "Food."},
+    ]
+
+    assert actual == expected
