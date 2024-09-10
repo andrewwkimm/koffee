f0d67f62e58d4ba0d17b7301a0812b98788bbea5
Andrew Kim <andrewkimka@gmail.com>
2024-09-10 16:45:19 +0000

    Support dynamically inputting Git username and email

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Prompt user for Git username and email
+read -p "Enter your Git username: " git_username
+read -p "Enter your Git email: " git_email
+
 # Configure baseline git settings
-git config --global --add safe.directory ${containerWorkspaceFolder}
-git config --global user.email "andrewkimka@gmail.com"
-git config --global user.name "Andrew Kim"
+git config --global --add safe.directory "${containerWorkspaceFolder}"
+git config --global user.email "$git_email"
+git config --global user.name "$git_username"
 
 # Install font that supports East Asian languages
-# Use `fc-list :lang=ko` to verify font is valid
-# outside the container.
+# Use `fc-list :lang=ko` to verify font is valid outside the container.
 sudo cp examples/fonts/NotoSerifCJK-Bold.ttc /usr/share/fonts
 sudo fc-cache -f -v
 
