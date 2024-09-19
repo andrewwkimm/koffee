a55322e5b076c66981cc1a56422b5d4f3befe93e
Andrew Kim <andrewkimka@gmail.com>
2024-09-19 02:52:12 +0000

    Add error handling for invalid video files

--- src/koffee/translate.py
+++ src/koffee/translate.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from typing import Optional, Union
 
 from koffee.asr import transcribe_text
+from koffee.exceptions import InvalidVideoFileError
 from koffee.overlay import overlay_subtitles
 from koffee.translator import translate_transcript
 from koffee.utils.text_to_srt_converter import convert_text_to_srt
@@ -26,20 +27,23 @@ def translate(
 ) -> Path:
     """Processes a video file for translation and subtitle overlay."""
     log.info("Processing video...")
+    try:
+        output_path = get_output_path(video_file_path, output_dir, output_name)
 
-    output_path = get_output_path(video_file_path, output_dir, output_name)
+        transcript = transcribe_text(
+            video_file_path, batch_size, compute_type, device, model
+        )
+        translated_transcript = translate_transcript(transcript, target_language)
+        translated_srt_file = convert_text_to_srt(translated_transcript)
 
-    transcript = transcribe_text(
-        video_file_path, batch_size, compute_type, device, model
-    )
-    translated_transcript = translate_transcript(transcript, target_language)
-    translated_srt_file = convert_text_to_srt(translated_transcript)
+        overlay_subtitles(video_file_path, translated_srt_file, output_path)
 
-    overlay_subtitles(video_file_path, translated_srt_file, output_path)
+        translated_srt_file.unlink()
 
-    translated_srt_file.unlink()
-
-    return output_path
+        return output_path
+    except RuntimeError as error:
+        error_message = "Inputted file is not a valid video file or does not exist."
+        raise InvalidVideoFileError(error_message) from error
 
 
 def get_output_path(
