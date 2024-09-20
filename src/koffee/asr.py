79445a2e89a550f3e632d0c816c8c47bf65e2fa8
Andrew Kim <andrewkimka@gmail.com>
2024-09-20 02:28:45 +0000

    Suppress nonsensical messages from various packages

--- src/koffee/asr.py
+++ src/koffee/asr.py
@@ -1,10 +1,13 @@
 """Text extractor from audio."""
 
+import contextlib
 import logging
 from pathlib import Path
 from typing import Union
 
-import whisperx
+logging.getLogger("speechbrain").setLevel(logging.ERROR)
+
+import whisperx  # noqa: E402
 
 
 log = logging.getLogger(__name__)
@@ -18,24 +21,29 @@ def transcribe_text(
     model: str,
 ) -> dict:
     """Transcribes text from a video file."""
+    logging.getLogger("pytorch_lightning").setLevel(logging.ERROR)
+
     log.info("Transcribing text.")
 
-    loaded_model = whisperx.load_model(
-        whisper_arch=model, device=device, compute_type=compute_type
-    )
-    audio = whisperx.load_audio(video_file)
-    transcript = loaded_model.transcribe(audio, batch_size=batch_size)
+    # Redirect stdout to suppress overly verbose messages from whisperx
+    with open("/dev/null", "w") as null_fd:
+        with contextlib.redirect_stdout(null_fd):
+            loaded_model = whisperx.load_model(
+                whisper_arch=model, device=device, compute_type=compute_type
+            )
+            audio = whisperx.load_audio(video_file)
+            transcript = loaded_model.transcribe(audio, batch_size=batch_size)
 
-    model_a, metadata = whisperx.load_align_model(
-        language_code=transcript["language"], device=device
-    )
-    aligned_transcript = whisperx.align(
-        transcript["segments"],
-        model_a,
-        metadata,
-        audio,
-        device,
-        return_char_alignments=False,
-    )
-    aligned_transcript["language"] = transcript["language"]
-    return aligned_transcript
+            model_a, metadata = whisperx.load_align_model(
+                language_code=transcript["language"], device=device
+            )
+            aligned_transcript = whisperx.align(
+                transcript["segments"],
+                model_a,
+                metadata,
+                audio,
+                device,
+                return_char_alignments=False,
+            )
+            aligned_transcript["language"] = transcript["language"]
+            return aligned_transcript
