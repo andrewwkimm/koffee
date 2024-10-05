0d4296110a980a87375493cdc4048bc32d0dcc25
Andrew Kim <andrewkimka@gmail.com>
2024-10-05 21:12:46 +0000

    Start data model for koffee configuration

---
+++ src/koffee/data/config.py
@@ -0,0 +1,19 @@
+"""The koffee Configuration."""
+
+from pathlib import Path
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class koffeeConfig(BaseModel):
+    """Configuration data model for koffee."""
+
+    batch_size: int = 16
+    compute_type: str = "float32"
+    device: str = "cpu"
+    model: str = "large-v3"
+    output_dir: Optional[Path] = None
+    output_name: Optional[str] = None
+    srt: Optional[bool] = False
+    target_language: str = "en"
