cdc8b415ca5d209929514f35112ea573c406d15a
Andrew Kim <andrewkimka@gmail.com>
2024-10-08 07:01:24 +0000

    Add an error for invalid subtitle formats

--- src/koffee/exceptions.py
+++ src/koffee/exceptions.py
@@ -1,5 +1,9 @@
 """Exceptions for koffee."""
 
 
+class InvalidSubtitleFormatError(Exception):
+    """Invalid subtitle format error."""
+
+
 class InvalidVideoFileError(Exception):
     """Input file is not a valid video file error."""
