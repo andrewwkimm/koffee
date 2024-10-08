ff9367d02ad0443ab64440be21b3721400c87052
Andrew Kim <andrewkimka@gmail.com>
2024-10-08 05:45:16 +0000

    Fix bug that caused the subtitle files having two additional lines

--- src/koffee/utils/vtt_converter.py
+++ src/koffee/utils/vtt_converter.py
@@ -11,23 +11,25 @@ from koffee.utils.timestamp_converter import convert_to_timestamp
 log = logging.getLogger(__name__)
 
 
-def convert_text_to_vtt(text: list, output_file_path: Optional[Path] = None) -> Path:
-    """Converts translated text to VTT format."""
+def convert_text_to_vtt(
+    transcript: list, output_file_path: Optional[Path] = None
+) -> Path:
+    """Converts text to VTT format."""
     log.info("Converting text to VTT format.")
 
     if output_file_path is None:
         output_file_path = Path(f"{os.getcwd()}/subtitle.vtt")
 
     with open(output_file_path, "w", encoding="utf-8") as file:
-        file.write("WEBVTT\n\n")  # VTT files start with a 'WEBVTT' header
+        file.write("WEBVTT\n\n")
 
-        for idx, subtitle in enumerate(text, 1):
+        for idx, subtitle in enumerate(transcript, 1):
             start = convert_to_timestamp(subtitle["start"], "vtt")
             end = convert_to_timestamp(subtitle["end"], "vtt")
             text = subtitle["text"]
 
             file.write(f"{start} --> {end}\n")
-            if idx != len(text):
+            if idx != len(transcript):
                 file.write(f"{text}\n\n")
             else:
                 file.write(f"{text}\n")
