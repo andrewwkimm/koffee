820e8b7e424b208bb8b21fdb74b5d1e65e53d204
Andrew Kim <andrewkimka@gmail.com>
2024-10-08 04:07:41 +0000

    Specify which subtitle format for timestamp conversion

--- src/koffee/utils/vtt_converter.py
+++ src/koffee/utils/vtt_converter.py
@@ -1,4 +1,4 @@
-"""Text to SRT converter."""
+"""Text to VTT converter."""
 
 import logging
 import os
@@ -11,20 +11,21 @@ from koffee.utils import convert_to_timestamp
 log = logging.getLogger(__name__)
 
 
-def convert_text_to_srt(text: list, output_file_path: Optional[Path] = None) -> Path:
-    """Converts translated text to SRT format."""
-    log.info("Converting text to SRT format.")
+def convert_text_to_vtt(text: list, output_file_path: Optional[Path] = None) -> Path:
+    """Converts translated text to VTT format."""
+    log.info("Converting text to VTT format.")
 
     if output_file_path is None:
-        output_file_path = Path(f"{os.getcwd()}/translated_text.srt")
+        output_file_path = Path(f"{os.getcwd()}/subtitle.vtt")
 
     with open(output_file_path, "w", encoding="utf-8") as file:
+        file.write("WEBVTT\n\n")  # VTT files start with a 'WEBVTT' header
+
         for idx, subtitle in enumerate(text, 1):
-            start = convert_to_timestamp(subtitle["start"], "srt")
-            end = convert_to_timestamp(subtitle["end"], "srt")
+            start = convert_to_timestamp(subtitle["start"], "vtt")
+            end = convert_to_timestamp(subtitle["end"], "vtt")
             text = subtitle["text"]
 
-            file.write(f"{idx}\n")
             file.write(f"{start} --> {end}\n")
             if idx != len(text):
                 file.write(f"{text}\n\n")
