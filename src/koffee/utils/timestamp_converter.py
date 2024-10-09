c9f9de02645aad3a61631cf144fd1d75d06e977b
Andrew Kim <andrewkimka@gmail.com>
2024-10-09 04:47:48 +0000

    Add logs

--- src/koffee/utils/timestamp_converter.py
+++ src/koffee/utils/timestamp_converter.py
@@ -1,13 +1,19 @@
 """Float to timestamp converter."""
 
 from datetime import timedelta
+import logging
 from typing import Union
 
 from koffee.exceptions import InvalidSubtitleFormatError
 
 
+log = logging.getLogger(__name__)
+
+
 def convert_to_timestamp(seconds: Union[float, int], subtitle_format: str) -> str:
     """Converts seconds to SRT timestamp format."""
+    log.debug("subtitle_format: " + repr(subtitle_format))
+
     ms = int((seconds % 1) * 1000)
     ts = timedelta(seconds=int(seconds))
     hours, remainder = divmod(ts.seconds, 3600)
