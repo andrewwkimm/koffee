e3b96e8ed6732ccc618aac00f057a2b0c0a20874
Andrew Kim <andrewkimka@gmail.com>
2024-10-10 08:57:16 +0000

    Test with the subtitles generated from a sample video

--- tests/test_subtitle.py
+++ tests/test_subtitle.py
@@ -22,11 +22,30 @@ def test_subtitle_generator(
     output_dir: Path,
 ) -> None:
     """Tests that the text is converted to the correct subtitle format."""
-    sample_text = [
-        {"start": 10.5, "end": 15.0, "text": "Hello, world!"},
-        {"start": 20.0, "end": 25.3, "text": "This is a subtitle."},
+    text = [
+        {
+            "start": 0.0,
+            "end": 6.28,
+            "text": "When we got out of the long tunnel of the border, it was an eyebled.",
+        },
+        {
+            "start": 7.8,
+            "end": 10.74,
+            "text": "The bottom of the night has been changed.",
+        },
+        {
+            "start": 12.32,
+            "end": 14.94,
+            "text": "The train stopped at the signal station.",
+        },
+        {
+            "start": 16.98,
+            "end": 24.06,
+            "text": "On the other side, a virgin approached and opened a window in front of the Shimmura.",
+        },
     ]
-    subtitle_file_path = generate_subtitles(subtitle_format, sample_text, output_dir)
+
+    subtitle_file_path = generate_subtitles(subtitle_format, text, output_dir)
 
     assert subtitle_file_path.exists()
 
@@ -35,21 +54,35 @@ def test_subtitle_generator(
 
     if subtitle_format == "srt":
         expected = """1
-00:00:10,500 --> 00:00:15,000
-Hello, world!
+00:00:00,000 --> 00:00:06,280
+When we got out of the long tunnel of the border, it was an eyebled.
 
 2
-00:00:20,000 --> 00:00:25,300
-This is a subtitle.
+00:00:07,799 --> 00:00:10,740
+The bottom of the night has been changed.
+
+3
+00:00:12,320 --> 00:00:14,939
+The train stopped at the signal station.
+
+4
+00:00:16,980 --> 00:00:24,059
+On the other side, a virgin approached and opened a window in front of the Shimmura.
 """
     elif subtitle_format == "vtt":
         expected = """WEBVTT
 
-00:00:10.500 --> 00:00:15.000
-Hello, world!
+00:00:00.000 --> 00:00:06.280
+When we got out of the long tunnel of the border, it was an eyebled.
 
-00:00:20.000 --> 00:00:25.300
-This is a subtitle.
+00:00:07.799 --> 00:00:10.740
+The bottom of the night has been changed.
+
+00:00:12.320 --> 00:00:14.939
+The train stopped at the signal station.
+
+00:00:16.980 --> 00:00:24.059
+On the other side, a virgin approached and opened a window in front of the Shimmura.
 """
     else:
         error_message = f"Invalid or unsupported subtitle format: {subtitle_format}"
