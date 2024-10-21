9450059a18a1939048e632657c77b7727a2d9010
Andrew Kim <andrewkimka@gmail.com>
2024-10-21 23:35:24 +0000

    Specify output directory and name

--- examples/video_translation_example.py
+++ examples/video_translation_example.py
@@ -3,7 +3,9 @@
 import koffee
 
 
-video_file_path = "examples/videos/sample_korean_video.mp4"
-
 if __name__ == "__main__":
+    video_file_path = "examples/videos/sample_korean_video.mp4"
+    output_dir = "scratch"
+    output_name = "example_video"
+
     koffee.translate(video_file_path)
