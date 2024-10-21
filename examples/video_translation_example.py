247a9745e812f47eb150ee2224a4e4c3cadf0dda
Andrew Kim <andrewkimka@gmail.com>
2024-10-21 23:20:31 +0000

    Start minimal working example

---
+++ examples/video_translation_example.py
@@ -0,0 +1,9 @@
+"""Example usage for video translation."""
+
+import koffee
+
+
+video_file_path = "examples/videos/sample_korean_video.mp4"
+
+if __name__ == "__main__":
+    koffee.translate(video_file_path)
