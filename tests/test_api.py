96bc7a4e9debddf9e307cd7d19f1a97ba2ac0fb6
andrewwkimm <andrewkimka@gmail.com>
2025-10-16 04:04:30 +0000

    Test Python API call

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,27 @@
 """Tests for the koffee API."""
 
+from pathlib import Path
+
 import pytest
 
 import koffee
 from koffee.exceptions import InvalidVideoFileError
+
+video_file_path = Path("examples/videos/sample_korean_video.mp4")
+output_directory_path = Path("scratch")
+output_file_name = "python_output_video_file"
+
+
+def test_api() -> None:
+    """Tests if the API call successfully outputs a file."""
+    output_video_file = koffee.translate(
+        video_file_path=video_file_path,
+        output_dir=output_directory_path,
+        output_name=output_file_name,
+    )
+
+    output_video_file_path = Path(output_video_file)
+    assert output_video_file_path.exists()
 
 
 def test_invalid_video_file() -> None:
