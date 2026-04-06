bcdf584361b5053e673fbae8706b87af437fa210
andrewwkimm <andrewkimka@gmail.com>
2026-04-06 18:05:59 -0400

    Add embedded subtitle track detection and extraction utilities

--- src/koffee/utils/__init__.py
+++ src/koffee/utils/__init__.py
@@ -2,6 +2,7 @@
 
 from .get_video_duration import get_video_duration
 from .srt_converter import convert_text_to_srt
+from .subtitle_extractor import extract_subtitle_track, get_subtitle_tracks
 from .subtitle_parser import parse_subtitle_file
 from .timestamp_converter import convert_to_timestamp
 from .vtt_converter import convert_text_to_vtt
@@ -10,6 +11,8 @@ __all__ = [
     "convert_text_to_srt",
     "convert_text_to_vtt",
     "convert_to_timestamp",
+    "extract_subtitle_track",
+    "get_subtitle_tracks",
     "get_video_duration",
     "parse_subtitle_file",
 ]
